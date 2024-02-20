86d530622127bf9cbc45f036ac7cc390b309a19e
Igor Sousa <igormsousa2003@gmail.com>
2024-02-20 08:10:29 -0300

    adicionando data

---
+++ interfaces/src/data/data-home.js
@@ -0,0 +1,8 @@
+const data_home = {
+    titulo: "Lista Dinâmica",
+    sub_titulo: "Bem-vindo à Página Inicial",
+    apresentacao_projeto: "Esta é a nova tela do projeto.",
+    botao: "Adicionar"
+}
+
+export default data_home;
--- interfaces/src/pages/Home.js
+++ interfaces/src/pages/Home.js
@@ -1,5 +1,7 @@
 // src/components/Home.js
 import React, { useState } from 'react';
+import '../styles/Home.css';
+import data_home from '../data/data-home';
 import List from '../components/List';
 import ItemCount from '../components/ItemCount';
 
@@ -24,12 +26,12 @@ function Home() {
 
   return (
     <div>
-      <h1>Lista Dinâmica</h1>
-      <h2>Bem-vindo à Página Inicial</h2>
-      <p>Esta é a nova tela do projeto.</p>
+      <h1>{data_home.titulo}</h1>
+      <h2>{data_home.sub_titulo}</h2>
+      <p>{data_home.apresentacao_projeto}</p>
       <form onSubmit={handleSubmit}>
         <input type="text" value={text} onChange={handleChange} />
-        <button type="submit">Adicionar</button>
+        <button type="submit">{data_home.botao}</button>
       </form>
       <ItemCount count={items.length} />
       <List items={items} handleDelete={handleDelete} />
