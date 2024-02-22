b259c7b809d93fdbc331de6f2781764b88798f7b
Igor Sousa <igormsousa2003@gmail.com>
2024-02-22 09:24:24 -0300

    consumacao de api

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,7 +1,54 @@
+import { useState, useEffect } from "react";
+
+const movieURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
+
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+
+    const res = await fetch(url);
+    const data = await res.json();
+    const dataResults = data.results;
+
+    console.log(dataResults);
+
+    const newData = dataResults.map((data) => {
+      return {
+        id: data.id,
+        original_language: data.original_language,
+        title: data.title,
+        overview: data.overview,
+        vote_average: data.vote_average,
+        poster_path: data.poster_path
+      }
+    });
+
+    setTopMovies(newData);
+
+    console.log(newData);
+
+  };
+
+  useEffect(() => {
+
+    const topRatedUrl = `${movieURL}top_rated?${apiKey}`;
+    console.log(topRatedUrl);
+
+    getTopRatedMovies(topRatedUrl);
+
+  }, []);
+
   return (
-    <div>
-      
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((topMovies) => <p>{topMovies.title}</p>)}
+      </div>
+
     </div>
   )
 }
